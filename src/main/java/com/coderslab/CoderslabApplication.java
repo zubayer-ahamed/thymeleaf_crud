311741cf449e8d611a047ad9ff46c90030fe3ade
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2017-12-22 16:54:33 +0600

    first commit

---
+++ src/main/java/com/coderslab/CoderslabApplication.java
@@ -0,0 +1,12 @@
+package com.coderslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderslabApplication.class, args);
+	}
+}
